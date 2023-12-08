6429021d793aa8a4523d7e56b11701fa9a663932
PagliacciPL <>
2023-12-08 13:14:17 +0100

    First commit

---
+++ XMLChecker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace XMLChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string xmlpath = args[0];
+                string xsdpath = args[1];
+                XmlSchemaSet schemas = new XmlSchemaSet();
+                schemas.Add("", xsdpath);
+                XDocument xmlfile = XDocument.Load(xmlpath);
+                bool error = false;
+                xmlfile.Validate(schemas, (o, e) =>
+                {
+                    Console.WriteLine("{0}", e.Message);
+                    error = true;
+                });
+                Console.WriteLine("{0}", error ? "XML is NOT correct against provided XSD" : "XML is correct against provided XSD");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0}", e.Message);
+            }
+
+        }
+    }
+}
